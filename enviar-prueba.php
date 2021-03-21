60f66aad4677f2b76cbe7d283cb5d6105423c1d8
ItCurso <80175518+nicky68622@users.noreply.github.com>
2021-03-21 13:34:22 -0300

    Add files via upload

--- enviar-prueba.php
+++ enviar-prueba.php
@@ -22,12 +22,12 @@ try {
     //Server settings
     $mail->SMTPDebug = 0;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'sv20.byethost20.org';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'itcursosonline2022@gmail.com';                     //SMTP username
+    $mail->Username   = 'adios';                     //SMTP username
     $mail->Password   = 'Lucyesme01';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->SMTPSecure = ssl;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 290;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
     $mail->setFrom($correo, $nombre);
